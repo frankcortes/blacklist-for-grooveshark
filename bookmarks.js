face54a7c0a6d596498be00232cc8f5dea1c5381
Francisco Cortés <franklyn.bdn@gmail.com>
2013-12-31 01:26:26 +0100

    Internal file for testing bookmark

--- bookmarks.js
+++
@@ -1,5 +0,0 @@
-(function() {
-	var scriptDep = document.createElement("script");
-	scriptDep.src = "file:///Users/franklyn/code/black list for Grooveshark/blacklist-api.js";
-	document.body.appendChild(scriptDep);
-})();
